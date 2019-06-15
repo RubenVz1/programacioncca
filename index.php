d6ecf087965ba5dbd48408336fa50b698f202077
RubenVz1 <hector_vz@outlook.es>
2019-06-15 00:18:57 -0500

    Se inicio el proyecto programacioncca, se añadieron las carpetas para la organizacion del codigo, se añadieron los archivos .html y .css para el inicio de sesion

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang = "es">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Inicio de sesion</title>
+		<link rel="stylesheet" href="styles/indexStylo.css">
+	</head>
+	<body>
+		<div id="inicioSesion">
+			<section id="cabecera">
+				<h1>Iniciar sesion</h1>
+			</section>
+			<section id="cuerpo">
+				<form action="">
+					<p>Usuario: </p><input type="text" name="usuario" value="Usuario"><br>
+					<p>Contraseña: </p><input type="text" name="usuario" value="Contraseña"><br>
+					<input id = "boton"type="submit" value="Iniciar sesion">
+				</form>
+			</section>
+		</div>
+	</body>
+</html>
+
+
