158fe9407aa2930454d3171572f65f636b6325be
RubenVz1 <hector_vz@outlook.es>
2019-07-16 01:57:48 -0500

    Se creo el calendario, se realizo la conexion con la base de datos para mostrar los datos en el calendario, se creo la pagina activity.php para mostrar la informacion de las actividades seleccionadas en el calendario

---
+++ vistas/activity.php
@@ -0,0 +1,58 @@
+<?php
+	include_once '../includes/user.php';
+    include_once '../includes/user_session.php';
+    include_once '../includes/db.php';
+    $userSession = new UserSession();
+    $user = new User();
+    if(isset($_SESSION['user']))
+    {
+        $user->setUser($userSession->getCurrentUser());
+    }
+    else
+    {
+        header("location: ../index.php");
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Calendario</title>
+        <link rel="stylesheet" href="../styles/calendarStylo.css">
+    </head>
+    <body>
+        <header>
+            <?php echo "<p>Bienvenido ".$user->getNombre()." con cargo ".$user->getCargo()."</p>"?>
+            <a id = botonRegresar" href="calendar.php">Regresar</a>
+            <a id = "boton" href="../includes/logout.php">Cerrar sesion</a>
+        </header>
+        <?php
+            $db = new DB();
+            if(isset($_GET['id']))
+            {
+                $id = $_GET['id'];
+                $query = $db->connect()->prepare("SELECT a.idActividad,r.nombreActividad,r.fechaProgramacion
+                                                  FROM actividad a, programacion p, requerimientoActividad r
+                                                  WHERE a.idProgramacion = p.idProgramacion
+                                                  AND p.idRequerimientoActividad = r.idRequerimientoActividad
+                                                  AND a.idActividad = $id");
+                $query->execute();
+                $query->setFetchMode(PDO::FETCH_NUM);
+                $result = $query->fetchAll();
+                if($query->rowCount())
+                {
+                    echo "<h1>".$result[0][1]."</h1>";
+                }
+                else
+                {
+                    echo "Hubo un error al cargar la avtividad";
+                }
+            }
+            else
+            {
+                header("location: ../vistas/calendar.php");
+            }
+            ?>
+    </body>
+</html>
--- vistas/calendar.php
+++ vistas/calendar.php
@@ -1,6 +1,7 @@
 <?php
 	include_once '../includes/user.php';
     include_once '../includes/user_session.php';
+    include_once '../includes/db.php';
     $userSession = new UserSession();
     $user = new User();
     if(isset($_SESSION['user']))
@@ -31,5 +32,126 @@
 			?>
 			<a id = "boton" href="../includes/logout.php">Cerrar sesion</a>
 		</header>
+		<div id="datos">
+			<table border="1" id="tabla">
+				<caption>Calendario</caption>
+				<tr>
+					<?php
+					function fechaAnterior($anterior)
+					{
+						if($anterior[0]==0)
+						{
+							$anterior[0]=12;
+							$anterior[1]-=1;
+						}
+						else
+							$anterior[0]-=1;
+						if($anterior[0]<10)
+							$anterior[0] = "0".$anterior[0];
+						return $anterior;
+					}
+
+					function fechaSiguiente($siguiente)
+					{
+						if($siguiente[0]>11)
+						{
+							$siguiente[0]=0;
+							$siguiente[1]+=1;
+						}
+						else
+							$siguiente[0]+=1;
+						if($siguiente[0]<10)
+							$siguiente[0] = "0".$siguiente[0];
+						return $siguiente;
+					}
+
+					function fechaString($dia,$mes,$anio)
+					{
+						if($dia<10)
+						{
+							$dia = "0".$dia;
+						}
+						return $anio."-".$mes."-".$dia;
+					}
+
+					$nombreMeses = array('Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre');
+					$fecha = new DateTime();
+					$mes = $fecha->format('m');
+					$anio = $fecha->format('Y');
+
+					$siguiente = fechaSiguiente(array($mes,$anio));
+					$anterior = fechaAnterior(array($mes,$anio));
+
+					if(isset($_GET['mes'])&&isset($_GET['anio']))
+					{
+						$siguiente = fechaSiguiente(array($_GET['mes'],$_GET['anio']));
+						$anterior = fechaAnterior(array($_GET['mes'],$_GET['anio']));
+						$mes = $_GET['mes'];
+						$anio = $_GET['anio'];
+					}
+					$db = new DB();
+					
+					$query = $db->connect()->prepare("SELECT a.idActividad,r.nombreActividad,r.fechaEvento
+			    						 		  FROM actividad a, programacion p, requerimientoActividad r
+			    						 		  WHERE a.idProgramacion = p.idProgramacion
+			    						 		  AND p.idRequerimientoActividad = r.idRequerimientoActividad
+			    						 		  AND r.fechaEvento >= '$anio-$mes-01'
+			    						 		  AND r.fechaEvento <= '$anio-$mes-31'");
+					$query->execute();
+					$query->setFetchMode(PDO::FETCH_NUM);
+				    $result = $query->fetchAll();
+					echo "<th id='atras'><a href='calendar.php?mes=".$anterior[0]."&anio=".$anterior[1]."'>atras</a></th>";
+					echo "<th id='fecha' colspan='5'>".$nombreMeses[$mes-1]." del ".$anio."</th>";
+					echo "<th id='siguiente'><a href='calendar.php?mes=".$siguiente[0]."&anio=".$siguiente[1]."'>siguiente</a></th>";
+					?>
+				</tr>
+				<tr>
+					<th>Lunes</th>
+					<th>Martes</th>
+					<th>Miercoles</th>
+					<th>Jueves</th>
+					<th>Viernes</th>
+					<th>Sabado</th>
+					<th>Domingo</th>
+				</tr>
+				<?php
+					$primeraCelda = date("w",mktime(0,0,0,$mes,1,$anio))+7;
+					$finMes = date("d",(mktime(0,0,0,$mes+1,1,$anio)-1));
+					$ultimaCelda = $primeraCelda + $finMes;
+					$contador = 1;
+					$dia = 1;
+					for($i=0;$i<6;$i++)
+					{
+						echo "<tr>";
+						for($j=0;$j<7;$j++)
+						{
+							echo "<th>";
+							if($contador >= $primeraCelda && $contador < $ultimaCelda)
+							{
+								echo $dia;
+								if($query->rowCount())
+								{
+									for($k = 0 ; $k < count($result) ; $k++)
+									{
+									    if($result[$k][2] == fechaString($dia,$mes,$anio))
+									    {
+									    	echo "<br><a href='../vistas/activity.php?id=".$result[$k][0]."'>".$result[$k][1]."</a><br>";
+									    }
+									}
+								}
+								$dia++;
+							}
+							else
+							{
+								echo "-";
+							}
+							echo "</th>";
+							$contador++;
+						}
+						echo "</tr>";
+					}
+				?>
+			</table>
+		</div>
 	</body>
 </html>
